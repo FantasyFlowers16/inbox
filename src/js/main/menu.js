0cbfc64e86b02a88c7c43b86a2d41270bde99209
Anastasia <fantasyflowers16@yandex.ru>
2021-11-18 18:45:31 +0700

    add animate

--- src/js/main/menu.js
+++ src/js/main/menu.js
@@ -2,6 +2,7 @@ class options  {
   constructor() {
     this.button = document.querySelector('.j-menu-but')
     this.container = document.querySelector('.j-menu-mobile')
+    this.header = document.querySelector('.j-header')
   }
 
 
@@ -10,12 +11,16 @@ class options  {
       if (this.button.classList.contains('_open')) {
         this.button.classList.remove('_open')
         this.container.classList.remove('_open')
+        this.header.classList.remove('_open-menu')
         document.documentElement.style.overflow = 'auto'
 
       }else {
         this.button.classList.add('_open')
         this.container.classList.add('_open')
-        this.container.style.top=window.pageYOffset+'px'
+        this.header.classList.add('_open-menu')
+        // this.container.scrollIntoView({block: "start", behavior: "smooth"});
+        // this.container.scrollTo(0,0)
+        // this.container.style.top=window.pageYOffset+'px'
         document.documentElement.style.overflow = 'hidden'
       }
     }
--- src/js/main/tasks.js
+++ src/js/main/tasks.js
@@ -2,11 +2,23 @@ class tasks  {
   constructor() {
     this.container = document.querySelector('.j-tasks')
     this.button = document.querySelector('.j-tasks-button')
-   
   }
 
   init() {
     this.button.onclick = () => {
+      var $this = this;
+      setTimeout(function() {
+        // $this.button.scrollIntoView({block: "start", behavior: "smooth"})
+        console.log($this.container.getBoundingClientRect().top)
+        var elementPosition = $this.button.getBoundingClientRect().top;
+        var offsetPosition = elementPosition - 67;
+        console.log(offsetPosition)
+        window.scrollBy({
+          top: offsetPosition,
+          left: 0,
+          behavior: 'smooth'
+        });
+      },300)
       if(this.button.classList.contains('_open')){
         this.button.classList.remove('_open')
         this.container.classList.remove('_open')
